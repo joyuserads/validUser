5b3ce30e7517817ad4832e1504ecfa8d7ea9d262
Joyce Silva <joyce.silva@nowseguros.seg.br>
2025-08-04 15:36:10 -0300

    validações insert

--- app.js
+++ app.js
@@ -4,18 +4,35 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
-const bycript = require('bycript');
+const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
 
 
-
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
 
+app.use(express.json());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    
+    console.log('Banco de dados conectado com sucesso');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+};
+
+
+
+app.get('/', (req, res) => {
+  res.send('API funcionando!');
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
-  
+
 });
--- saas/middlewares/auth.js
+++ saas/middlewares/auth.js
@@ -1,5 +1,20 @@
-const jwt = require('')
+const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next  ) =>{
-    const authorization = req.header
-}
+const authMiddleware = (req, res, next) => {
+    const authorization = req.header('Authorization');
+    if (!authorization) {
+        return res.status(401).json({ message: 'Token não fornecido' });
+    }
+
+    const token = authorization.replace('Bearer ', '');
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token inválido' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
